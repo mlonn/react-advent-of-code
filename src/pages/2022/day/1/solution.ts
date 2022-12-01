ee03438552723a3ace061a9ba782ac12c361e553
Mikael Lönn <mikael@sendify.se>
2022-12-01 11:00:30 +0100

    day one

--- src/pages/2022/day/1/solution.ts
+++ src/pages/2022/day/1/solution.ts
@@ -1,40 +1,32 @@
 import { lines } from "../../../../utils/helpers";
 
 export const part1 = (input: string) => {
-  let max = 0;
+  const sums: number[] = [];
   let sum = 0;
   lines(input).forEach((line) => {
     const number = parseInt(line);
     if (!number) {
-      if (sum > max) {
-        max = sum;
-      }
+      sums.push(sum);
       sum = 0;
     } else {
       sum += number;
     }
   });
-  return max;
+  return Math.max(...sums);
 };
 export const part2 = (input: string) => {
-  let max: number[] = [0];
-
+  const sums: number[] = [];
   let sum = 0;
   lines(input).forEach((line) => {
     const number = parseInt(line);
-    if (number) {
-      sum += number;
-    } else {
-      if (sum > max[max.length - 1]) {
-        max.push(sum);
-        max.sort((a, b) => b - a);
-        if (max.length > 3) {
-          max.pop();
-        }
-      }
+    if (!number) {
+      sums.push(sum);
       sum = 0;
+    } else {
+      sum += number;
     }
   });
-  console.log(max);
-  return max.reduce((m, a) => m + a, 0);
+  sums.sort((a, b) => b - a);
+
+  return sums[0] + sums[1] + sums[2];
 };
