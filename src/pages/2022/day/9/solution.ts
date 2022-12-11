519ffba46db32f1d04514869c213f8acd3b367f9
Mikael Lönn <mikael@sendify.se>
2022-12-11 16:49:06 +0100

    day9

--- src/pages/2022/day/9/solution.ts
+++ src/pages/2022/day/9/solution.ts
@@ -1,6 +1,87 @@
-export const part1 = (input: string) => {
-  return "Not yet solved";
+import { lines, Point } from "../../../../utils/helpers";
+
+type Direction = "U" | "R" | "D" | "L";
+const Directions: { [key in Direction]: Point } = {
+  U: { x: 0, y: -1 },
+  R: { x: 1, y: 0 },
+  D: { x: 0, y: 1 },
+  L: { x: -1, y: 0 },
 };
+
+interface Rope {
+  p: Point;
+  tail?: Rope;
+}
+const moveHead = (h: Point, dir: Direction) => {
+  h.x += Directions[dir].x;
+  h.y += Directions[dir].y;
+};
+const moveTail = (h: Point, t: Point, dir: Direction) => {
+  if (h.y === t.y && Math.abs(h.x - t.x) === 2) {
+    t.x += h.x - t.x > 0 ? 1 : -1;
+  }
+  if (h.x === t.x && Math.abs(h.y - t.y) === 2) {
+    t.y += h.y - t.y > 0 ? 1 : -1;
+  }
+  if (t.x === h.x) {
+    return;
+  }
+  if (t.y === h.y) {
+    return;
+  }
+  if (Math.abs(h.y - t.y) === 1 && Math.abs(h.x - t.x) === 1) {
+    return;
+  }
+  t.x += h.x - t.x > 0 ? 1 : -1;
+  t.y += h.y - t.y > 0 ? 1 : -1;
+};
+
+const moveRope = (rope: Rope, dir: Direction, visited: any) => {
+  if (rope.tail) {
+    moveTail(rope.p, rope.tail.p, dir);
+    moveRope(rope.tail, dir, visited);
+  } else {
+    visited[rope.p.x + "," + rope.p.y] = true;
+  }
+};
+
+export const part1 = (input: string) => {
+  const h = { x: 0, y: 0 };
+  const t = { x: 0, y: 0 };
+  const visited: any = {};
+  lines(input).forEach((line) => {
+    const [dir, dist] = line.split(" ");
+    for (let i = 0; i < parseInt(dist); i++) {
+      moveHead(h, dir as Direction);
+      moveTail(h, t, dir as Direction);
+      visited[t.y + "," + t.x] = true;
+    }
+  });
+
+  return Object.keys(visited).length;
+};
+
+const createRope = (length: number) => {
+  const rope: Rope = { p: { x: 0, y: 0 } };
+  let currentRope = rope;
+  for (let i = 0; i < length; i++) {
+    const newRope = { p: { x: 0, y: 0 } };
+    currentRope.tail = newRope;
+    currentRope = newRope;
+  }
+  return rope;
+};
+
 export const part2 = (input: string) => {
-  return "Not yet solved";
+  const rope = createRope(9);
+  const visited: any = {};
+  lines(input).forEach((line) => {
+    const [dir, dist] = line.split(" ");
+    for (let i = 0; i < parseInt(dist); i++) {
+      moveHead(rope.p, dir as Direction);
+      moveRope(rope, dir as Direction, visited);
+    }
+  });
+
+  return Object.keys(visited).length;
 };
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,8 @@
+export interface Point {
+  x: number;
+  y: number;
+}
+
 export function permute<T>(permutation: T[]): T[][] {
   let length = permutation.length,
     result = [permutation.slice()],
